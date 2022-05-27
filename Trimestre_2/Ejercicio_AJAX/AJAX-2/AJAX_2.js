aade21f1430b1b5884db219d0a7140921457b290
AlvaroAMGX <91255818+AlvaroAMGX@users.noreply.github.com>
2022-05-27 18:12:41 +0200

    Add files via upload

---
+++ Trimestre_2/Ejercicio_AJAX/AJAX-2/AJAX_2.js
@@ -0,0 +1,14 @@
+//Temperatura máxima
+document.getElementsByTagName('temperatura')[0].childNodes[1].childNodes[0].nodeValue
+//Temperatura mínima
+document.getElementsByTagName('temperatura')[2].childNodes[1].childNodes[0].nodeValue
+//Estado del cielo
+document.getElementsByTagName('estado_cielo')[2].attributes[1].value
+//direccion del viento
+document.getElementsByTagName('viento')[2].childNodes[1].childNodes[0].nodeValue
+//Velocidad del viento
+document.getElementsByTagName('viento')[2].childNodes[3].childNodes[0].nodeValue
+//Humedad relativa máxima
+document.getElementsByTagName('humedad_relativa')[0].childNodes[1].childNodes[0].nodeValue
+//Humedad relativa minima
+document.getElementsByTagName('humedad_relativa')[0].childNodes[3].childNodes[0].nodeValue
